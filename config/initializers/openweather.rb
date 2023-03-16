2fc36e8b83f2e15d621c34e68a601b6951d96265
mmkclement <maungmaungkha18290@gmail.com>
2023-03-16 11:42:05 +0100

    heroku test2

--- config/initializers/openweather.rb
+++ config/initializers/openweather.rb
@@ -3,6 +3,14 @@
 require 'open-uri'
 require 'net/https'
 
+OpenSSL::SSL::VERIFY_PEER = OpenSSL::SSL::VERIFY_NONE if Rails.env.development?
+
+OpenWeather.configure do |config|
+  config.api_key = ENV['OPENWEATHER_API_KEY']
+  config.adapter = Faraday.default_adapter
+  config.http_adapter.ssl.verify = true
+end
+
 module Net
   class HTTP
     alias_method :original_use_ssl=, :use_ssl=
@@ -18,5 +26,3 @@ end
 OPENWEATHER_CLIENT = OpenWeather::Client.new(
   api_key: ENV["OPENWEATHER_API_KEY"]
 )
-
-
